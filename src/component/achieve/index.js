deaa28dc07ccf2a407480dad136a551323bbd382
SalmanAhmed24 <salman.ahmed.abbasi.24@gmail.com>
2023-08-29 11:38:09 +0500

    made the changes in about us page

--- src/component/achieve/index.js
+++ src/component/achieve/index.js
@@ -1,7 +1,12 @@
 import Image from "next/image";
 import "./style.scss";
-
+import React, { useState, useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 function Achieve({ futuraBK, futuraMDBT, futuraLtBT, futuraHv, poppins }) {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <section className="ach-wrap">
       <h1 className={poppins.className}>Our Achievements</h1>
@@ -9,8 +14,27 @@ function Achieve({ futuraBK, futuraMDBT, futuraLtBT, futuraHv, poppins }) {
         GrowthEcho is the world&apos;s Top Branded seller and has grown the
         Amazon business to $6 Million in one year
       </p>
-      <Image alt="growth eco" src={"/achieve.png"} width={1370} height={350} />
-      <img className="mobile-roc" src={"/mobile-roc-ach.png"} />
+      <Image
+        data-aos="fade-right"
+        data-aos-offset="00"
+        data-aos-delay="50"
+        data-aos-duration="1000"
+        data-aos-easing="ease-in-out"
+        className="header-content-wrap"
+        alt="growth eco"
+        src={"/achieve.png"}
+        width={1370}
+        height={350}
+      />
+      <img
+        data-aos="fade-right"
+        data-aos-offset="00"
+        data-aos-delay="50"
+        data-aos-duration="1000"
+        data-aos-easing="ease-in-out"
+        className="mobile-roc"
+        src={"/mobile-roc-ach.png"}
+      />
     </section>
   );
 }
