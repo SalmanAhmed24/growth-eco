43848385c3ea2f348dd25397a7ab2e7219f580fb
SalmanAhmed24 <salman.ahmed.abbasi.24@gmail.com>
2023-09-07 10:41:15 +0500

    added counter animation

--- src/component/serviceComponents/service-counter.js
+++ src/component/serviceComponents/service-counter.js
@@ -1,8 +1,12 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import "./serviceComp.scss";
 import SliderSubComp from "./sliderSubComp";
 import { Poppins } from "next/font/google";
+import React, { useState } from "react";
+import CountUp from "react-countup";
+import ScrollTrigger from "react-scroll-trigger";
 const poppins = Poppins({
   weight: ["300", "400", "600", "700", "900"],
   style: ["normal", "italic"],
@@ -17,6 +21,7 @@ function ServiceCounter({
   headingSub2,
   paraSub2,
 }) {
+  const [counterFlag, setCounterFlag] = useState(false);
   return (
     <section className="ser-counter">
       <div className="counter-info-wrap">
@@ -26,12 +31,29 @@ function ServiceCounter({
         </div>
       </div>
       <div className="slider-ser-wrap">
+        <ScrollTrigger
+          onEnter={() => setCounterFlag(true)}
+          onExit={() => setCounterFlag(false)}
+        >
+          <div className="singleItem">
+            <h1 className={poppins.className}>
+              {counterFlag ? (
+                <CountUp duration={5} startOnMount={false} end={600} />
+              ) : (
+                "600"
+              )}
+            </h1>
+            <p className={poppins.className}>{paraSub1}</p>
+          </div>
+        </ScrollTrigger>
         <div className="singleItem">
-          <h1 className={poppins.className}>{headingSub1}</h1>
-          <p className={poppins.className}>{paraSub1}</p>
-        </div>
-        <div className="singleItem">
-          <h1 className={poppins.className}>{headingSub2}</h1>
+          <h1 className={poppins.className}>
+            {counterFlag ? (
+              <CountUp duration={5} startOnMount={false} end={7} />
+            ) : (
+              "7"
+            )}
+          </h1>
           <p className={poppins.className}>{paraSub2}</p>
         </div>
       </div>
